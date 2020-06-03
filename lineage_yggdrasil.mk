aa3f4da462bfd2d7cec16ed31ee5317f03daf77c
Adam Pigg <adam@piggz.co.uk>
2020-06-03 22:41:27 +0100

    Inherit from the correct products to build sailfish
    Change-Id: I70e492fa0daedcdd78c2d191796da69e6e3f5a49

--- lineage_yggdrasil.mk
+++ lineage_yggdrasil.mk
@@ -7,13 +7,13 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from Volla Phone (yggdrasil) device
 $(call inherit-product, device/volla/yggdrasil/device.mk)
 
 # Inherit some common lineage stuff.
-# $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_yggdrasil
