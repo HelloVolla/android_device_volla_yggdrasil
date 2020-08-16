4a23d2a02f17ea53d4eebd31082ccb0702384df6
TheKit <nekit1000@gmail.com>
2020-08-16 12:12:02 +0300

    halium-overlay: run audiosystem-passthrough in AudioFlinger mode

--- device.mk
+++ device.mk
@@ -57,6 +57,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/halium-overlay/lib/udev/rules.d/70-android.rules:$(TARGET_COPY_OUT_SYSTEM)/halium/lib/udev/rules.d/70-android.rules \
     $(LOCAL_PATH)/halium-overlay/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android9:$(TARGET_COPY_OUT_SYSTEM)/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android9 \
+    $(LOCAL_PATH)/halium-overlay/usr/share/upstart/sessions/audiosystem-passthrough.conf:$(TARGET_COPY_OUT_SYSTEM)/halium/usr/share/upstart/sessions/audiosystem-passthrough.conf \
     $(LOCAL_PATH)/halium-overlay/usr/sbin/mount-android.sh:$(TARGET_COPY_OUT_SYSTEM)/halium/usr/sbin/mount-android.sh \
     $(LOCAL_PATH)/halium-overlay/etc/init/bluebinder.conf:$(TARGET_COPY_OUT_SYSTEM)/halium/etc/init/bluebinder.conf \
     $(LOCAL_PATH)/halium-overlay/etc/init/lxc-android-config.override:$(TARGET_COPY_OUT_SYSTEM)/halium/etc/init/lxc-android-config.override \
